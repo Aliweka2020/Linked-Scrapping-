722d6e0e2cdb0f186f7ec8fcb24614e784107347
Ali Elhenidy <aweka220@YAHOO.COM>
2025-08-13 13:10:41 +0300

    main

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Linked-Scrapping-
-This project extracts and processes LinkedIn profile data using LangChain. By combining smart web scraping with language model capabilities, it automatically collects structured details like job titles, work history, skills, and education, transforming them into actionable insights.
+# Ice Breaker
---
+++ agents/linkedin_lookup_agent.py
@@ -0,0 +1,53 @@
+import os
+from dotenv import load_dotenv
+
+from tools.tools import get_profile_url_tavily
+
+load_dotenv()
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain.agents import (
+create_react_agent,
+AgentExecutor,
+)
+from langchain import hub
+from langchain_ollama import ChatOllama
+
+def lookup(name:str)->str:
+    '''
+    llm = ChatOpenAI(
+        temperature=0,
+        model_name="gpt-3.5-turbo",
+    )
+    '''
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    #llm = ChatOllama(model = "gpt-oss:20b")
+    template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
+                                  Your answer should contain only a URL"""
+
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google 4 linkedin profile page",
+            func=get_profile_url_tavily,
+            description="useful for when you need get the Linkedin Page URL",
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True, handle_parsing_errors=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linked_profile_url = result["output"]
+    return linked_profile_url
+
+if __name__ == "__main__":
+    linkedinurl = lookup(name = "Ahmed Elhelow")
+    print(linkedinurl)
